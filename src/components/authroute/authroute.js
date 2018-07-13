bf5984843abfc8c9d878639a30973f8cae3684ec
YuYEZhou <1182756902@qq.com>
2018-07-13 09:57:02 +0800

    user information check

--- src/components/authroute/authroute.js
+++ src/components/authroute/authroute.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import axios from 'axios'
+import { withRouter } from 'react-router-dom'
 
+@withRouter
 class AuthRoute extends React.Component{
   componentDidMount() {
+    const pubilcList = ['/login', '/register']
+    const pathname = this.props.location.pathname
+    if (pubilcList.indexOf(pathname) > -1){
+      return null
+    }
     // 获取用户信息
     axios.get('/user/info')
       .then(res=>{
         if (res.status === 200){
-          console.log(res.data)
+          if (res.data.code === 0){
+
+          }else{
+            this.props.history.push('/login')
+          }
         }
       })
     // 是否登录
