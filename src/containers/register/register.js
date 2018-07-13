e36a694b8dd603840bf018b763f40724adaf9390
YuYEZhou <1182756902@qq.com>
2018-07-13 10:35:46 +0800

    user register interaction

--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -6,8 +6,22 @@ class Register extends React.Component{
   constructor(props) {
     super(props)
     this.state = {
-      type:'genius'
+      user: '',
+      pwd: '',
+      repeatpwd: '',
+      type: 'genius'
     }
+    this.hanleButton = this.hanleButton.bind(this)
+  }
+
+  hanleChange(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+
+  hanleButton() {
+    console.log(this.state);    
   }
 
   render() {
@@ -17,17 +31,33 @@ class Register extends React.Component{
         <Logo />
         <WhiteSpace />
         <List>
-          <InputItem>用户名</InputItem>
+          <InputItem
+            onChange={(v) => this.hanleChange('user', v)}
+          >用户名</InputItem>
           <WhiteSpace />
-          <InputItem>密码</InputItem>
+          <InputItem
+           type="password"
+           onChange={(v) => this.hanleChange('pwd', v)}
+          >密码</InputItem>
           <WhiteSpace />
-          <InputItem>确认密码</InputItem>
+          <InputItem
+           type="password"
+           onChange={(v) => this.hanleChange('repeatpwd', v)}
+          >确认密码</InputItem>
           <WhiteSpace />
-          <RadioItem checked={this.state.type === 'genius'}>牛人</RadioItem>
+          <RadioItem
+           onChange={() => this.hanleChange('type', 'genius')}
+           checked={this.state.type === 'genius'}
+          >牛人</RadioItem>
           <WhiteSpace />
-          <RadioItem checked={this.state.type === 'boss'}>BOSS</RadioItem>
+          <RadioItem
+           onChange={() => this.hanleChange('type', 'boss')}
+           checked={this.state.type === 'boss'}
+          >BOSS</RadioItem>
           <WhiteSpace />
-          <Button type="primary">注册</Button>
+          <Button type="primary"
+            onClick={this.hanleButton}
+          >注册</Button>
         </List>
         
       </div>
