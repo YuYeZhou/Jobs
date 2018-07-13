8250250c98156c39d179b2078c7cab009fe71445
YuYEZhou <1182756902@qq.com>
2018-07-13 21:14:05 +0800

    achieve registration function

--- server/model.js
+++ server/model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 // 链接mongo 并且使用jobs这个集合
-const DB_URL = 'mongodb://http://localhost:27017/jobs-chat'
-mongoose.connect(DB_URL)
+const DB_URL = 'mongodb://localhost:27017/jobs-chat'
+mongoose.connect(DB_URL, {useNewUrlParser:true})
 
 const models = {
   user: {
--- server/user.js
+++ server/user.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const Router = express.Router()
+const bodyParser = require('body-parser')
+const cookParser = require('cookie-parser')
 const model = require('./model')
 const User = model.getModel('user')
+
 
 Router.get('/list', (req, res) =>{
   User.find({}, (err, doc) => 
     res.json(doc)
   )
 })
+
+Router.post('/register', (req, res) =>{
+  console.log(req.body.data)
+  const {user, pwd, type} = req.body.data
+  User.findOne({user}, (err, doc) => {
+    if(doc) {
+      return res.json({ code: 1, msg: '用户名重复' })
+    }
+    User.create({ user, pwd, type }, (err, data) => {
+      if(err) {
+        return res.json({ code: 1, msg: '后端出错了' })
+      }
+      return res.json({ code: 0 })
+    })
+  })
+})
+
 Router.get('/info', (req, res) => 
   // 用户没有cookie
   res.json({code:1})
