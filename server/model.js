2d415e1c37bab030bfac7708ce05e9231c2356ce
YuYEZhou <1182756902@qq.com>
2018-07-13 20:29:42 +0800

    set database model

---
+++ server/model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+// 链接mongo 并且使用jobs这个集合
+const DB_URL = 'mongodb://http://localhost:27017/jobs-chat'
+mongoose.connect(DB_URL)
+
+const models = {
+  user: {
+    user: {'type': String, 'require': true},
+    pwd: {'type': String, 'require': true},
+    type: {'type': String, 'require': true},
+    //头像
+    'avatar': {'type': String},
+    //个人简介或者职位简介
+    'title': {'type': String},
+    // 如果你是boss 还有两个字段
+    'company': {'type': String},
+    'money': {'type': String}
+  },
+  chat: {}
+}
+
+for (let m in models) {
+  mongoose.model(m, new mongoose.Schema(models[m]))
+}
+
+module.exports = {
+  getModel: function(name) {
+    return mongoose.model(name)
+  }
+}
--- server/user.js
+++ server/user.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const Router = express.Router()
+const model = require('./model')
+const User = model.getModel('user')
 
+Router.get('/list', (req, res) =>{
+  User.find({}, (err, doc) => 
+    res.json(doc)
+  )
+})
 Router.get('/info', (req, res) => 
   // 用户没有cookie
   res.json({code:1})
