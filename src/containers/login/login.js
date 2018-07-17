17392d630ed7829d8928d5519341a5e3b508ef67
YuYEZhou <1182756902@qq.com>
2018-07-17 23:06:42 +0800

    hoc form

--- src/containers/login/login.js
+++ src/containers/login/login.js
@@ -4,13 +4,14 @@ import { List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { login } from '../../redux/user.redux'
+import Form from '../../components/form/form'
 import '../../index.css'
 
 @connect(
   state => state.user,
   {login}
 )
-
+@Form
 class Login extends React.Component{
   constructor(props) {
     super(props)
@@ -21,15 +22,9 @@ class Login extends React.Component{
     this.handleLogin = this.handleLogin.bind(this)
     this.register = this.register.bind(this)
   }
-
-  handleChange(key, val) {
-    this.setState({
-      [key]: val
-    })
-  }
-
+  
   handleLogin() {
-    this.props.login(this.state)
+    this.props.login(this.props.state)
   }
 
   register() {
@@ -47,12 +42,12 @@ class Login extends React.Component{
         <List>
           {this.props.msg?<p className="error-msg">{this.props.msg}</p>:null}
           <InputItem
-           onChange={(v) => this.handleChange('user', v)}
+           onChange={(v) => this.props.handleChange('user', v)}
           >用户</InputItem>
           <WhiteSpace />
           <InputItem
            type="password"
-           onChange={(v) => this.handleChange('pwd', v)}
+           onChange={(v) => this.props.handleChange('pwd', v)}
           >密码</InputItem>
         </List>
         <WhiteSpace />
--- src/containers/register/register.js
+++ src/containers/register/register.js
@@ -4,32 +4,26 @@ import { WhiteSpace, List, InputItem, Radio, Button } from 'antd-mobile'
 import { Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { register } from '../../redux/user.redux'
+import Form from '../../components/form/form'
 import '../../index.css'
 
 @connect(
   state => state.user,
   {register}
 )
+@Form
 class Register extends React.Component{
   constructor(props) {
     super(props)
-    this.state = {
-      user: '',
-      pwd: '',
-      repeatpwd: '',
-      type: 'genius'
-    }
     this.handleRegister = this.handleRegister.bind(this)
   }
 
-  handleChange(key, val) {
-    this.setState({
-      [key]: val
-    })
+  componentDidMount(){
+    this.props.handleChange('type', 'genius')
   }
 
   handleRegister() {
-    this.props.register(this.state)
+    this.props.register(this.props.state)
   }
 
   render() {
@@ -42,27 +36,27 @@ class Register extends React.Component{
         <List>
           {this.props.msg?<p className="error-msg">{this.props.msg}</p>:null}
           <InputItem
-            onChange={(v) => this.handleChange('user', v)}
+            onChange={(v) => this.props.handleChange('user', v)}
           >用户名</InputItem>
           <WhiteSpace />
           <InputItem
            type="password"
-           onChange={(v) => this.handleChange('pwd', v)}
+           onChange={(v) => this.props.handleChange('pwd', v)}
           >密码</InputItem>
           <WhiteSpace />
           <InputItem
            type="password"
-           onChange={(v) => this.handleChange('repeatpwd', v)}
+           onChange={(v) => this.props.handleChange('repeatpwd', v)}
           >确认密码</InputItem>
           <WhiteSpace />
           <RadioItem
-           onChange={() => this.handleChange('type', 'genius')}
-           checked={this.state.type === 'genius'}
+           onChange={() => this.props.handleChange('type', 'genius')}
+           checked={this.props.state.type === 'genius'}
           >牛人</RadioItem>
           <WhiteSpace />
           <RadioItem
-           onChange={() => this.handleChange('type', 'boss')}
-           checked={this.state.type === 'boss'}
+           onChange={() => this.props.handleChange('type', 'boss')}
+           checked={this.props.state.type === 'boss'}
           >BOSS</RadioItem>
           <WhiteSpace />
           <Button type="primary"
