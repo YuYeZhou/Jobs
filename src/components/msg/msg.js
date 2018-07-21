7d771c183eca3fb69ef5acdb2996a367b565637d
YuYEZhou <1182756902@qq.com>
2018-07-21 08:19:43 +0800

    unreading and sorting

--- src/components/msg/msg.js
+++ src/components/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { List } from 'antd-mobile'
+import { List, Badge } from 'antd-mobile'
 
 @connect(
   state=>state
@@ -19,19 +19,29 @@ class Msg extends React.Component{
       msgGroup[v.chatid] = msgGroup[v.chatid] || []
       msgGroup[v.chatid].push(v)
     })
-    const chatList = Object.values(msgGroup)
+    const chatList = Object.values(msgGroup).sort((a, b) => {
+      const a_last = this.getLast(a).create_time
+      const b_last = this.getLast(b).create_time
+      return b_last - a_last
+    })
     return(
       <div>
           {chatList.map(v => {
             const lastItem = this.getLast(v)
             const targetId = v[0].from === userid? v[0].to:v[0].from
+            const unreadNum = v.filter(v => !v.read && v.to === userid).length
             if(!userinfo[targetId]){ return null }
             // const name = userinfo[targetId]?userinfo[targetId].name:''
             // const avatar = userinfo[targetId]?userinfo[targetId].avatar:''
             return (
               <List key={lastItem._id}>
                 <Item
+                  extra={<Badge text={unreadNum}></Badge>}
                   thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
+                  arrow="horizontal"
+                  onClick={() => {
+                    this.props.history.push(`/chat/${targetId}`)
+                  }}
                 >
                   {lastItem.content}
                   <Brief>{userinfo[targetId].name}</Brief>
